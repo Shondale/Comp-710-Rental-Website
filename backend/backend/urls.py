86c045b2f731e9ea9cd0d0ec5bfcb26cf07139db
Ubuntu <ubuntu@ip-172-31-15-221.us-east-2.compute.internal>
2021-04-26 22:04:35 +0000

    safety commit

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import hello, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/<name>', hello),
+    #path('savetodb/<name>', saveToDb),
+    path('', index),
 ]
